c75f5bcd5fa983b1b0adcd1948d7a2aad08e2668
Marlon Maniti <lonmaniti@gmail.com>
2015-10-26 14:49:36 +1300

    Removed Loading and added error page

--- js/main.js
+++ js/main.js
@@ -1,12 +1,2 @@
 
-
-$(window).load(function() {
-    
-	setTimeout(
-	  function() {
-	    $('#pulse-wrapper').fadeOut();
-	    window.sr = new scrollReveal();
-	  }, 3000);
-
-});
-
+window.sr = new scrollReveal();
