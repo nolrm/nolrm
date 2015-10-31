c8c0fe9cf5b3980b8aa5cf22351b34437f5aee39
Marlon Maniti <lonmaniti@gmail.com>
2015-10-31 13:59:15 +1300

    Menu ajax call

--- js/main.js
+++ js/main.js
@@ -1,2 +1,31 @@
 
+$(document).ready(function() {
+
+	var menu 				 = $('.masthead__menu'),
+			intro 				 = $('.main-intro'),
+			screenHeight = $(window).height();
+
+	intro.css('height', screenHeight + "px")
+
+	console.log(screenHeight)
+
+	menu.on('click', function() {
+
+		$.ajax({
+		  url: 'http://nolrm.github.io/menu',
+		  success:function(data){
+		  	var response = $(data),
+						result = response.filter('.menu').html();
+		    
+		    console.log(result);
+
+		    $('#main').html(result).fadeIn();
+		    // console.log(data);
+		  }
+		});
+
+	});
+
+});
+
 window.sr = new scrollReveal();
