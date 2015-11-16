2ee73aa6e7389f1c661d466e9bf89fd1609fa633
Marlon Maniti <lonmaniti@gmail.com>
2015-11-16 20:25:31 +1300

    Finish static and SASS for styleguide

--- source/mcmaniti/config.rb
+++ source/mcmaniti/config.rb
@@ -1,4 +1,5 @@
 # Require any additional compass plugins here.
+# require 'bootstrap-sass'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
@@ -9,14 +10,14 @@ images_dir = "img"
 javascripts_dir = "js"
 fonts_dir = "fonts"
 
-output_style = :nested
+# output_style = :nested
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
-color_output = false
+line_comments = false
+# color_output = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
