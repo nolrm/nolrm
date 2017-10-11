6a71db2bf02ddc642a2169e034fde3ef2c9377c7
Mrlon <mrlon@Mrlons-MacBook-Pro.local>
2017-10-11 22:07:55 +1300

    theme switcher

--- js/main.js
+++ js/main.js
@@ -3,4 +3,12 @@
 
     window.sr = new scrollReveal();
 
+    $('.theme-switcher-dark').on('click', function() {
+      $('.js-css-url').attr('href', '/css/ui-dark.css')
+    });
+
+    $('.theme-switcher-light').on('click', function() {
+      $('.js-css-url').attr('href', '/css/ui-light.css')
+    });
+
 }());
